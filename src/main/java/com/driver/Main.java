144d5e214db91273e37f53d8943f7473dd02a991
Ahmed Lafeef EN <lafeefen@gmail.com>
2022-10-31 17:19:05 +0530

    polymorphism added

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,21 @@
 package com.driver;
-
+class Product{
+    public int product(int x, int y) {
+        return x*y;
+    }
+    public int product(int x, int y, int z) {
+        return x*y*z;
+    }
+    public double product(double x, double y) {
+        return x*y;
+    }
+}
 public class Main {
+    public static void main(String[] args) {
+        Product p = new Product();
+        System.out.println(p.product(4,5));
+        System.out.println(p.product(4,5,6));
+        System.out.println(p.product(2.3,5.6));
+    }
 
 }
